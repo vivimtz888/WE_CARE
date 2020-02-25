764499bea564bdb26c2ed7d8716c3589fb4fbabd
alec quig <alecoralec@gmail.com>
2020-02-25 17:23:26 -0600

    Finish home page view

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,4 @@
 class Room < ApplicationRecord
-    belongs_to :user
     has_many :bookings
     has_many :users, through: :booking, dependent: :destroy
     validates :name, :price, presence: true
