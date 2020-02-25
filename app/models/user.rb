b0dd84bd860bc36a6d7d667661254dd1908f6b83
Juan Carlos Banos Mafud <juancarlosbanosmafud@gmail.com>
2020-02-25 16:30:45 -0600

    Finished the seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
-  has_many :rooms, through: :bookings
+  has_many :rooms, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
