8a15d2813941238eb921d45cbc53f97b78d6ce76
alec quig <alecoralec@gmail.com>
2020-02-28 11:28:47 -0600

    move map beneath date fields

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,7 @@
 import "bootstrap";
-
 import { initFlatpickr } from "../plugins/init_flatpickr";
-
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 initMapbox();
 
