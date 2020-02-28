072ac133544ccb4ab440a89a4f8da567a91e2edd
Juan Carlos Banos Mafud <juancarlosbanosmafud@gmail.com>
2020-02-28 14:08:53 -0600

    Finished the final seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,30 +13,38 @@ Booking.destroy_all
 
 puts "creating owners and their rooms"
 
-choices = ["old,room", "scary,room", "modern,room", "cozy,room", "luxury,room", "mansion,room", "hotel,room", "terry,crews"]
+addresses = ["Campestre 233, Ciudad de Mexico", "Francia 82, Ciudad de Mexico", "Margaritas 100, Ciudad de Mexico", "Camelia 22, Ciudad de Mexico", "Naranjo 85, Ciudad de Mexico", "Insurgentes 3000, Ciudad de Mexico"]
 
+images_url = ['https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80',
+              'https://images.unsplash.com/photo-1550581190-9c1c48d21d6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=998&q=80',
+              'https://images.unsplash.com/photo-1537726235470-8504e3beef77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1558442074-3c19857bc1dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
+              'https://images.unsplash.com/photo-1542889601-399c4f3a8402?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1560448205-4d9b3e6bb6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1553881651-43348b2ca74e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1486304873000-235643847519?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
+              'https://images.unsplash.com/photo-1544207240-42895ede7c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1553444892-20174939d7bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1575918265268-99dd8f4d6254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+              'https://images.unsplash.com/photo-1553444836-bc6c8d340ba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+              'https://images.unsplash.com/photo-1546555648-fb7876c40c58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
+              'https://images.unsplash.com/photo-1572891086295-6c1c7c476549?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1900&q=80'
+]
 
-10.times do
+5.times do
   user = User.new(email: Faker::Internet.email, password: '123456', owner: true)
   user.save
-  rand(1..4).times do
-    url = "https://source.unsplash.com/collection/2303151/#{rand(1..9999)}"
-    Room.create(name: Faker::FunnyName.four_word_name, address: Faker::Address.country, price: rand(100000), user_id: user.id, photo_url:"#{url}")
-  end
+end
+
+images_url.each do |image|
+  Room.create(name: Faker::Company.name, address: addresses.sample, price: rand(1000), user_id: User.all.sample.id, photo_url: images_url.sample)
 end
 
 puts "creating non owners"
-10.times do
+5.times do
   user = User.new(email: Faker::Internet.email, password: '123456', owner: false  )
   user.save
 end
 
-# Booking first room
-first_room = Room.first
 
-Booking.create!(
-  user: User.last,
-  start_date: Date.today + 4.days,
-  end_date: Date.today + 7.days,
-  room: first_room
-)
